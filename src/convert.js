a288574e6d9756fdfbe04d1519ae597afde4b86a
Max Fechner <max.fechner@gmail.com>
2019-12-22 14:56:20 +0100

    ...

--- src/convert.js
+++ src/convert.js
@@ -5,9 +5,10 @@ const words = JSON.parse(FS.readFileSync('words.json','utf8'));
 
 for (let word of words) {
 	let SVG = base.replace('TEXT', word[0]).replace('COLOR', word[1]);
+	let filename = word[0].replace('?', '').replace('!', '').replace("'","").replace(" ","");
 	sharp(Buffer.from(SVG))
 		.png()
-		.toFile('../dist/' + word[0].replace('?', '').replace('!', '') + ".png")
+		.toFile('../dist/' + filename + ".png")
 		.then(function (info) {
 			console.log(info)
 		})
