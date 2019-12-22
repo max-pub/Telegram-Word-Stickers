49b9dc3e7c8307b79397f5ee74463128ac927608
Max Fechner <max.fechner@gmail.com>
2019-12-22 14:50:06 +0100

    init

---
+++ src/convert.js
@@ -0,0 +1,17 @@
+const sharp = require("sharp")
+const FS = require('fs');
+const base = FS.readFileSync('base.svg', 'utf8');
+const words = JSON.parse(FS.readFileSync('words.json','utf8'));
+
+for (let word of words) {
+	let SVG = base.replace('TEXT', word[0]).replace('COLOR', word[1]);
+	sharp(Buffer.from(SVG))
+		.png()
+		.toFile('../dist/' + word[0].replace('?', '').replace('!', '') + ".png")
+		.then(function (info) {
+			console.log(info)
+		})
+		.catch(function (err) {
+			console.log(err)
+		})
+}
